1f45fe9a861735a4b62a278f4fb81786994644cd
Ajinkya Dhaigude <ad8454@rit.edu>
2017-02-18 00:12:56 -0500

    add ImageFrameActivity

---
+++ app/src/main/java/edu/rit/ad8454/dandyhack/ImageFrameActivity.java
@@ -0,0 +1,13 @@
+package edu.rit.ad8454.dandyhack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ImageFrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_frame);
+    }
+}
