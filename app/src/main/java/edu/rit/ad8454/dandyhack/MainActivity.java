60c1a9aa4f7ea1921dcd51322210a6a00d7ec788
Ajinkya Dhaigude <ad8454@rit.edu>
2017-02-17 22:30:24 -0500

    testing first commit

--- app/src/main/java/edu/rit/ad8454/dandyhack/MainActivity.java
+++ app/src/main/java/edu/rit/ad8454/dandyhack/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.rit.ad8454.dandyhack;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button takePhoto = (Button) findViewById(R.id.takePhoto);
+        takePhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (photoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(photoIntent, 1);
+                }
+            }
+        });
     }
 }
